61214222e379b084c712b0d90eb337abb688a8c5
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-10-29 21:10:22 -0400

    Created data file, dynamic

--- js/script.js
+++ js/script.js
@@ -3,7 +3,26 @@
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 console.log(isMobile);
 
+// Portfolio item class
+function PortfolioItem() {
+	this.id = 0;
+	this.name = "";
+	this.category = "";
+	this.thumbnail = "";
+	this.embed = "";
+	this.link = "";
+	this.title = "";
+	this.description = "";
+}
+
 $(document).ready(function(){
+
+	// Read in JSON
+	var portfolio_data;
+	$.getJSON("bin/portfolio.json", function(json) {
+		portfolio_data = json;
+		setup();
+	});
 
 	var NUM_HIGHLIGHTS = 15;
 	// Lets openFrameworks this shiz
@@ -30,12 +49,22 @@ $(document).ready(function(){
 			});
 		});
 
+		// Setup portfolio section
+		for (var i = 0; i < portfolio_data.length; i++) {
+			$('#'+portfolio_data[i].category).append("<div class='portfolioEntry' portfolio_data-id="+portfolio_data[i].id+"></div>");
+			$('#'+portfolio_data[i].category+' .portfolioEntry').last().append("<img src='bin/portfolio_thumbnails/"+portfolio_data[i].thumbnail+"'><img>");
+		};
+
+		add_handlers();
 		// $("body").css('width', $(window).width());
-		// window.setTimeout(function(){update()}, 500);
+		window.setTimeout(function(){change_section()}, 500);
 	}
 
-	function update() {
+	function change_section() {
 		// HACK AWAY!
+
+		$("#mainScreen").empty();
+
 		var $selected = $("#mainNavigation .selected");
 		var selectedWidth = $selected.width();
 
@@ -49,28 +78,59 @@ $(document).ready(function(){
 		});
 	}
 
-	// Handlers!
-	$("#mainNavigation div").on('click', function(e){
-		$("#mainNavigation .selected").removeClass("selected");
-		$('.section').css('display', 'none');
-		$(this).addClass("selected");
-		var textid = '#' + $(this).children('h2').html().toLowerCase();
-		$(textid).css('display', 'block')
-		update();
-	});
-	// $("#mainNavigation").on('mousemove', function(e){
-	// 	$("#underline .highlight").first()
-	// 		.css("left", e.offsetX);
-	// });
-	$("#mainNavigation div").on('mouseenter', function(e){
-		var self = this;
-		$("#underline .highlight").first()
-			.css("left", $(self).position().left - 2);
-	});
+	function set_main_screen(project){
+		$("#mainScreen").empty().append("<iframe src="+project.embed+" frameborder='0' allowfullscreen>></iframe><p class='description'></p>");
+		var iframeWidth = $('#mainScreen iframe').width();
+		$("#mainScreen iframe").css('height', iframeWidth*9/16);
+		$("#mainScreen p").append("<strong>"+project.title+"</strong><br>"+project.description);
+	}
 
-	// window.onresize = function(e){
-	// 	setup();
-	// });
+	function add_handlers(){
 
-	setup();
+		// Handlers!
+		$("#mainNavigation div").on('click', function(e){
+			$("#mainNavigation .selected").removeClass("selected");
+			$('.section').css('display', 'none');
+			$(this).addClass("selected");
+			var textid = '#' + $(this).children('h2').html().toLowerCase();
+			$(textid).css('display', 'block')
+			change_section();
+		});
+		// $("#mainNavigation").on('mousemove', function(e){
+		// 	$("#underline .highlight").first()
+		// 		.css("left", e.offsetX);
+		// });
+		$("#mainNavigation div").on('mouseenter', function(e){
+			var self = this;
+			$("#underline .highlight").first()
+				.css("left", $(self).position().left - 2);
+		});
+
+		$('.portfolioEntry').on({
+			mouseenter: function(e){
+				$(this).velocity({
+					opacity: 0.5});
+			},
+			mouseleave: function(e) {
+				$(this).velocity({
+					opacity: 1});
+			},
+			click: function(e) {
+				var data_id = $(this).attr('portfolio_data-id');
+				var project;
+				for (var i = 0; i < portfolio_data.length; i++) {
+					if( portfolio_data[i].id == data_id ) {
+						project = portfolio_data[i]; 
+					}
+				};
+				set_main_screen(project);
+			}
+		});
+
+		// window.onresize = function(e){
+		// 	setup();
+		// });
+
+	}
+
 });
